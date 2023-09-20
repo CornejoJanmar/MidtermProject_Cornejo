20ddedad24fb220896e52f3c6aa4b28dd2d6d9d9
Local Pizza Employee <LocalPizzaEmployee@users.noreply.github.com>
2023-09-21 00:19:49 +0800

    Added: Material, Cinemachine, Input System, and Enemy AI Script

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Unity.AI;
 
 public class AIMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavMeshAgent navMeshAgent;
+    public Transform centerPoint;
+    public float range;
+
+    private void Start()
     {
-        
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();     
+    }
+    
+    private void Update()
+    {  
+
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            Vector3 point;
+            if(RandomPoint(centerPoint.position, range, out point))
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                navMeshAgent.SetDestination(point);
+            }
+        }
+         
     }
 
-    // Update is called once per frame
-    void Update()
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
-        
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if(NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
     }
 }
